e3d3ac9fb4df4e8a62b7212bbb428bc2ad6b852c
chloeskt <chloe.sekkat@ensae.fr>
2022-03-29 18:40:40 +0200

    add zero shot evaluation on xquad

---
+++ sh_scripts/qa/evaluate_xquad.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ../env/bin/activate
+
+rm -rf canine_mva
+git clone https://"${GITHUB_TOKEN}"@github.com/chloeskt/canine_mva.git
+cd canine_mva
+
+LANGUAGES=('xquad.en' 'xquad.ar' 'xquad.de' 'xquad.zh' 'xquad.vi' 'xquad.es' 'xquad.hi' 'xquad.el' 'xquad.th' 'xquad.tr' 'xquad.ru' 'xquad.ro')
+
+for lang in "${LANGUAGES[@]}"; do \
+    python source/qa/evaluate_xquad.py \
+      --model_path /mnt/hdd/canine/models/canine-c/... \
+      --language "$lang" \
+      --squad_v2 \
+      --max_answer_length 256 \
+      --max_length 2048 \
+      --doc_stride 512 \
+      --n_best_size 20 \
+      --batch_size 12 ;
+done
