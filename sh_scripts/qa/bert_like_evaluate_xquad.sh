40bcbc6b0d3ea5807bf3d57593a9b5eb1ca195dd
chloeskt <chloe.sekkat@ensae.fr>
2022-04-03 16:57:41 +0200

    add loop

--- sh_scripts/qa/bert_like_evaluate_xquad.sh
+++ sh_scripts/qa/bert_like_evaluate_xquad.sh
@@ -10,14 +10,18 @@ pip install .
 cd examples/pytorch/question-answering
 pip install -r requirements.txt
 
-python run_qa.py \
-    --model_name_or_path /mnt/hdd/canine/models/mbert \
-    --dataset_name xquad \
-    --dataset_config_name xquad.th \
-    --do_eval \
-    --n_best_size 20 \
-    --max_answer_length 30 \
-    --max_seq_length 384 \
-    --doc_stride 128 \
-    --output_dir /mnt/hdd/canine/models/mbert/ \
-    --per_device_eval_batch_size=6  \
+LANGUAGES=('xquad.en' 'xquad.ar' 'xquad.de' 'xquad.zh' 'xquad.vi' 'xquad.es' 'xquad.hi' 'xquad.el' 'xquad.th' 'xquad.tr' 'xquad.ru' 'xquad.ro')
+
+for lang in "${LANGUAGES[@]}"; do \
+  python run_qa.py \
+      --model_name_or_path /mnt/hdd/canine/models/mbert \
+      --dataset_name xquad \
+      --dataset_config_name xquad."$lang" \
+      --do_eval \
+      --n_best_size 20 \
+      --max_answer_length 30 \
+      --max_seq_length 384 \
+      --doc_stride 128 \
+      --output_dir /mnt/hdd/canine/models/mbert/ \
+      --per_device_eval_batch_size=6  \
+done
